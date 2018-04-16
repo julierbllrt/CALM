7197740edb7f983e20c814b65b52366fa496d056
Nirvador <x.herwyn@laposte.net>
2018-04-16 01:03:40 +0100

    profile

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,27 +1,22 @@
 /**
  * Created by Romain on 06/04/2017.
+ * Updated by Xavier on 16/04/2018
  */
-var mongoose = require('mongoose');
-//var User = mongoose.model('User');
+const mongoose = require('mongoose');
+
+// var User = mongoose.model('User');
 var User = require('../models/user');
 var Address = mongoose.model('Address');
 
-module.exports.profileRead = function(req, res) {
+module.exports.profileRead = function (req, res) {
   // If no user ID exists in the JWT return a 401
   if (!req.payload._id) {
-    res.status(401).json({
-      "message" : "UnauthorizedError: private profile"
-    });
+    res.status(401).json({ 'message': 'UnauthorizedError: private profile' });
   } else {
     // Otherwise continue
     User
       .findById(req.payload._id)
       .populate({path: 'address'})
-      .exec(function(err, user) {
-        res.json(user);
-      });
+      .exec(function (err, user) { res.json(user); });
   }
-
 };
-
-
