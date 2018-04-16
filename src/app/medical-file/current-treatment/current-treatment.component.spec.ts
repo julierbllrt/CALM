0737d63d6ec2200716ff8cc2641af9284e3f71ca
Julie Robillart <julie.robillart@isen.yncrea.fr>
2018-04-16 16:43:22 +0200

    version mobile tableau traitement current-treatment

--- src/app/medical-file/current-treatment/current-treatment.component.spec.ts
+++ src/app/medical-file/current-treatment/current-treatment.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+
 import { CurrentTreatmentComponent } from './current-treatment.component';
 
 describe('CurrentTreatmentComponent', () => {
